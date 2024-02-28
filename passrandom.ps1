ed62969642ca29ab84cc5099bea88b9af3d1ee6f
Manas Chuphal (WIPRO LIMITED) <v-mchuphal@microsoft.com>
2024-02-28 08:56:28 -0800

    Enter the changes

--- passrandom.ps1
+++ passrandom.ps1
@@ -1,9 +1,13 @@
-﻿# Generate a random password
+﻿#NEW PROGRAM
+
+#Generate a random password
 $length = 8
-// defining variable with length//
+##defining variable with length
 $characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
-//specifying character to the variables//
+##specifying character to the variables
 $password = -join (Get-Random -InputObject $characters -Count $length)
-//using the switch generating random input using assigned variables and locking the output to different variable.
+##using the switch generating random input using assigned variables and locking the output to different variable
 # Display the result
 Write-Host "Random password: $password"
+
+
